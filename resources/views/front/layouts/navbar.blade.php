cb09f5927662f47a24a1cde0314402c8d256bb4d
Mohammed-Ragab-Osman <mohamedragab7000@gmail.com>
2018-06-06 14:56:56 +0200

    no message

--- resources/views/front/layouts/navbar.blade.php
+++ resources/views/front/layouts/navbar.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-sm-6">
                 <div class="logo">
-                    <h1><a href=".."><img src="{{Storage::url(sett()->logo)}}"></a></h1>
+                    <h1><a href=".."><img src="{{Storage::url(sett()->logop)}}"></a></h1>
                 </div>
             </div>
 
