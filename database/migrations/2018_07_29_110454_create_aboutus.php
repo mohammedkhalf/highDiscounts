c8ff0aa68bac5dc2ab196131db21453f44f07ac8
hageribrahim <hageribrahim99@gmail.com>
2018-07-29 13:43:17 +0200

    pull

---
+++ database/migrations/2018_07_29_110454_create_aboutus.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAboutus extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aboutus', function (Blueprint $table){
+            $table->increments('id');
+            $table->text('description');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('aboutus');
+    }
+}
