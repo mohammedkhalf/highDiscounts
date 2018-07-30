4fa03e351f5aa62ca95aa5d61b07012eb46f224a
Mohammed-Ragab-Osman <mohamedragab7000@gmail.com>
2018-07-30 13:27:59 +0200

    no message

--- storage/framework/views/78835b74e1f552919f11e390f4532b334526ec54.php
+++ storage/framework/views/78835b74e1f552919f11e390f4532b334526ec54.php
@@ -6,12 +6,15 @@
                     <h1><a href=".."><img src="<?php echo e(url('/img/logo.png')); ?>"></a></h1>
                 </div>
             </div>
-
+            <?php   $product = App\Model\ShoppingCart::where('user_id','=',Auth::user()->id)->get()->all();
+        $total =  App\Model\ShoppingCart::where('user_id','=',Auth::user()->id)->sum('price'); ?>
+<?php if( $product != null): ?>
             <div class="col-sm-6">
                 <div class="shopping-item">
-                    <a href="<?php echo e(url('shopping-cart')); ?>">Cart - <span class="cart-amunt"></span> <i class="fa fa-shopping-cart"></i> <span class="product-count"><?php echo e(Session::has('cart') ? Session::get('cart')->totalQty : ''); ?></span></a>
+                    <a href="<?php echo e(url('shopping-cart')); ?>">Cart - <?php echo e($total); ?><span class="cart-amunt"></span> <i class="fa fa-shopping-cart"></i> <span class="product-count"> <?php echo e(count ($product)); ?> </span></a>
                 </div>
             </div>
+            <?php endif; ?>
         </div>
     </div>
 </div> <!-- End site branding area -->
