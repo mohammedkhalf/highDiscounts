154158a3b11bfa9dad264e7ad49660b390498142
Mohammed-Ragab-Osman <mohamedragab7000@gmail.com>
2018-08-26 15:11:56 +0200

    no message

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Model\DepartmentProducts as Dep;
+use App\Model\Products ;
+
+class SearchController extends Controller
+{
+    public function searchName(Request $request){
+
+    }
+}
